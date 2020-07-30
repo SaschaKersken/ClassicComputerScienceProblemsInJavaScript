2164c097c655e65e1182f2fa9590a2952c335441
Sascha Kersken <kersken@dimensional.de>
2020-07-30 22:42:42 +0200

    Node version of TTT game

--- chapter8/tictactoe.js
+++ chapter8/tictactoe.js
@@ -13,11 +13,11 @@ class TTTPiece {
 
   opposite() {
     if (this.value == X) {
-      this.value = O;
+      return new TTTPiece(O);
     } else if (this.value == O) {
-      this.value = X;
+      return new TTTPiece(X);
     } else {
-      this.value = E;
+      return new TTTPiece(E);
     }
   }
 }
@@ -44,7 +44,6 @@ class TTTBoard {
   }
 
   legalMoves() {
-    // return this.position.slice().filter((l) => l.value == E);
     let result = [];
     for (let i in this.position) {
       if (this.position[i].value == E) {
@@ -56,14 +55,14 @@ class TTTBoard {
 
   isWin() {
     // three row, three column, and then two diagonal checks
-    return (this.position[0].value == this.position[1] && this.position[0].value == this.position[2] && this.position[0].value != E) ||
-      (this.position[3].value == this.position[4] && this.position[3].value == this.position[5] && this.position[3].value != E) || 
-      (this.position[6].value == this.position[7] && this.position[6].value == this.position[8] && this.position[6].value != E) ||
-      (this.position[0].value == this.position[3] && this.position[0].value == this.position[6] && this.position[0].value != E) ||
-      (this.position[1].value == this.position[4] && this.position[1].value == this.position[7] && this.position[1].value != E) ||
-      (this.position[2].value == this.position[5] && this.position[2].value == this.position[8] && this.position[2].value != E) ||
-      (this.position[0].value == this.position[4] && this.position[0].value == this.position[8] && this.position[0].value != E) ||
-      (this.position[2].value == this.position[4] && this.position[2].value == this.position[6] && this.position[2].value != E);
+    return (this.position[0].value == this.position[1].value && this.position[0].value == this.position[2].value && this.position[0].value != E) ||
+      (this.position[3].value == this.position[4].value && this.position[3].value == this.position[5].value && this.position[3].value != E) || 
+      (this.position[6].value == this.position[7].value && this.position[6].value == this.position[8].value && this.position[6].value != E) ||
+      (this.position[0].value == this.position[3].value && this.position[0].value == this.position[6].value && this.position[0].value != E) ||
+      (this.position[1].value == this.position[4].value && this.position[1].value == this.position[7].value && this.position[1].value != E) ||
+      (this.position[2].value == this.position[5].value && this.position[2].value == this.position[8].value && this.position[2].value != E) ||
+      (this.position[0].value == this.position[4].value && this.position[0].value == this.position[8].value && this.position[0].value != E) ||
+      (this.position[2].value == this.position[4].value && this.position[2].value == this.position[6].value && this.position[2].value != E);
   }
 
   isDraw() {
--- chapter8/tictactoe_ai_node.js
+++ chapter8/tictactoe_ai_node.js
@@ -13,15 +13,16 @@ var board = new tt.TTTBoard();
 // Main game loop (recursion to use readline)
 var recursiveAsyncReadLine = function() {
   rl.question('Enter a legal square (0-8): ', function(humanMove) {
-    console.log(humanMove, board.legalMoves());
     if (board.legalMoves().indexOf(humanMove) > -1) {
       board = board.move(humanMove);
       if (board.isWin()) {
         util.out('Human wins!');
         rl.close();
+        return;
       } else if (board.isDraw()) {
         util.out('Draw!');
         rl.close();
+        return;
       }
       let computerMove = mm.findBestMove(board);
       util.out('Computer move is ' + computerMove);
@@ -30,9 +31,11 @@ var recursiveAsyncReadLine = function() {
       if (board.isWin()) {
         util.out('Computer wins!');
         rl.close();
+        return;
       } else if (board.isDraw()) {
         util.out('Draw!');
         rl.close();
+        return;
       }
     }
     recursiveAsyncReadLine();
